75b804ec6bfba2cc2e16940a4035a9eebfeba5d2
taoufiq <protips234@gmail.com>
2021-12-24 16:26:31 -0800

    update 1

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 const navBtn = document.querySelector(".fa-bars");
 const navContent = document.querySelector(".navContent");
+const navUl = document.querySelector(".navUl");
+const aboutbtn = document.querySelector(".about");
+const seachPlayer = document.querySelector(".seachPlayer");
+const loginBtn = document.querySelector(".login");
+const signUpBtn = document.querySelector(".signUp");
+var x = document.getElementsByTagName("BODY")[0];
 
-navBtn.addEventListener("click", ()=> {
-    navContent.classList.toggle("hide");
-})
+navBtn.addEventListener("click", () => {
+  navContent.classList.toggle("hide");
+});
+
+addEventListener("click", (e) => {
+  if (
+    e.target != navContent &&
+    e.target != aboutbtn &&
+    e.target != seachPlayer &&
+    e.target != loginBtn &&
+    e.target != signUpBtn &&
+    e.target != navBtn &&
+    e.target != navUl
+  ) {
+    navContent.classList.add("hide");
+  }
+});
